df383a2936150ed8850e5694085123e53ebbc1fe
MrElbow123 <82199893+MrElbow123@users.noreply.github.com>
2023-08-29 22:23:23 +0800

    Add files via upload

--- Test.R
+++ Test.R
@@ -2,8 +2,6 @@ library(Rcpp)
 library(HDInterval)
 sourceCpp('src/cat_mult_fw_cpp.cpp')
 
-#load("data/nonverbal.rda")
-#load("data/nonverbal_cov.rda")
 load("data/data.rda")
 load("data/data_cov.rda")
 load("data/emiss_mu_int_bar.rda")
@@ -15,7 +13,6 @@ source('R/logl_mnl.R')         # llmnl_int
 source('R/mnl_hess.R')
 source('R/mnl_RW_once.R')
 
-#sourceCpp('src/RcppExports.cpp')
 library(MCMCpack)
 library(mvtnorm)
 m <- 3
@@ -31,21 +28,12 @@ start_EM <- list(
                         ncol = q_emiss[1])
                 )
 
-n_subj <- 39
+n_subj <- 200
 n_time <- 233
 xt1 <- list(matrix(data_cov$sum_mess,ncol=n_subj,nrow=n_time))
 xt2 <- list(matrix(data_cov$sum_mess2,ncol=n_subj,nrow=n_time))
-xt3 <- list(matrix(data_cov$live_type,ncol=n_subj,nrow=n_time))
-xt4 <- list(matrix(data_cov$user_type,ncol=n_subj,nrow=n_time))
-xt5 <- list(matrix(data_cov$time_zone,ncol=n_subj,nrow=n_time))
-xt6 <- list(matrix(data_cov$time_gap,ncol=n_subj,nrow=n_time))
-xt7 <- list(matrix(data_cov$last_time,ncol=n_subj,nrow=n_time))
-xt8 <- list(matrix(data_cov$host_number,ncol=n_subj,nrow=n_time))
-xt9 <- list(matrix(data_cov$position,ncol=n_subj,nrow=n_time))
-xt <- c(xt1,xt2,xt3,xt4,xt5,xt6,xt7,xt8,xt9)
-
-xx <- rep( list(matrix( c(rep(1, n_subj),rowMeans(t(xt1[[1]])),rowMeans(t(xt2[[1]])),rowMeans(t(xt3[[1]])),rowMeans(t(xt4[[1]])),
-                          rowMeans(t(xt5[[1]])),rowMeans(t(xt6[[1]])),rowMeans(t(xt7[[1]])),rowMeans(t(xt8[[1]])),rowMeans(t(xt9[[1]]))
-                          ), ncol = 10, nrow = n_subj)), 1+n_dep)
+xt <- c(xt1,xt2)
+xx <- rep( list(matrix( c(rep(1, n_subj),rowMeans(t(xt1[[1]])),rowMeans(t(xt2[[1]]))
+                          ), ncol = 3, nrow = n_subj)), 1+n_dep)
 
 out_2st_c <- mHMM(s_data = data, xx = xx, xt = xt, emiss_mu_int_bar = emiss_mu_int_bar,gen = list(m = m, n_dep = n_dep, q_emiss = q_emiss),start_val = c(list(start_TM), start_EM),mcmc = list(J = 50000, burn_in = 40000))
